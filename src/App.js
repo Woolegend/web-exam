dadbe24e25732b6c8d0a6f185cfddf94c773930e
Woolegend <dnwogus4260@naver.com>
2025-05-21 11:31:56 +0900

    justify-content 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [justifyContent, setJustifyContent] = useState("flex-start");
+const JUSTIFY_CONTENT = {
+  start: {
+    text: "flex-start",
+    name: "justify-start",
+  },
+  center: {
+    text: "center",
+    name: "justify-center",
+  },
+  end: {
+    text: "flex-end",
+    name: "justify-end",
+  },
+  between: {
+    text: "space-between",
+    name: "justify-between",
+  },
+  around: {
+    text: "space-around",
+    name: "justify-around",
+  },
+};
 
-  const handleJustifyContentChange = (event) => {
-    setJustifyContent(event.target.value);
+function App() {
+  const [JC_KEY, setJC_KEY] = useState("start");
+
+  const handleClickJC = (event) => {
+    setJC_KEY(event.target.value);
   };
 
   return (
     <div className="App">
       <div id="container">
         <div className="wrapper">
-          <h2>justify-content : {justifyContent}</h2>
-          <div className={`flex-container ${justifyContent}`}>
+          <h2>justify-content : {JUSTIFY_CONTENT[JC_KEY].text}</h2>
+          <div className={`flex-container ${JUSTIFY_CONTENT[JC_KEY].name}`}>
             {Array.from({ length: 3 }).map((_, index) => (
               <div key={index} className="flex-item" />
             ))}
           </div>
-          <div className="button-list" onClick={handleJustifyContentChange}>
-            <button value="justify-start">flex-start</button>
-            <button value="justify-center">center</button>
-            <button value="justify-end">flex-end</button>
-            <button value="justify-between">space-between</button>
-            <button value="justify-around">space-around</button>
+          <div className="button-list">
+            {Object.keys(JUSTIFY_CONTENT).map((key) => (
+              <button key={key} value={key} onClick={handleClickJC}>
+                {JUSTIFY_CONTENT[key].text}
+              </button>
+            ))}
           </div>
         </div>
       </div>
