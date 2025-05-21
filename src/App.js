2439f3a5ca41eee9c976f076a32773f2a349d55d
Woolegend <dnwogus4260@naver.com>
2025-05-21 11:17:06 +0900

    justify content 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [justifyContent, setJustifyContent] = useState("flex-start");
+
+  const handleJustifyContentChange = (event) => {
+    setJustifyContent(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <div className="wrapper">
+          <h2>justify-content : {justifyContent}</h2>
+          <div className={`flex-container ${justifyContent}`}>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <div key={index} className="flex-item" />
+            ))}
+          </div>
+          <div className="button-list" onClick={handleJustifyContentChange}>
+            <button value="justify-start">flex-start</button>
+            <button value="justify-center">center</button>
+            <button value="justify-end">flex-end</button>
+            <button value="justify-between">space-between</button>
+            <button value="justify-around">space-around</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
